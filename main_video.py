a9567dc6be39eb4b430b8aca4c11be8bddf38bfc
MMsicarius <78599701+MMsicarius@users.noreply.github.com>
2022-06-07 06:04:31 +0100

    added new functionality to the detection of faces. Searching access credentials are broken

--- main_video.py
+++ main_video.py
@@ -14,7 +14,7 @@ while True:
     ret, frame = cap.read()
 
     # Detect Faces
-    face_locations, face_names = sfr.detect_known_faces(frame)
+    face_locations, face_names, assess_state = sfr.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
--- simple_facerec.py
+++ simple_facerec.py
@@ -9,62 +9,61 @@ class SimpleFacerec:
     def __init__(self):
         self.faces = []
         self.names = []
+        self.access = []
+        self.access_level = 0
 
         # Resize frame for a faster speed
         self.frame_resize_ratio = 0.25
-        self.credentials = pd.read_csv("access_credentials.csv")
+        self.credentials = pd.read_csv("access_credentials.csv", index_col="Index")
         self.access_log = pd.read_csv("access_log.csv")
+        # TODO credentials
+        print(self.credentials.loc[self.credentials["Index"] == 1])
+
+    def change_access_level(self):
+        print("Please specify the access level of the entrance:")
+        x = input()
+        if x.isnumeric():
+            self.access_level = x
+        else:
+            "Error! Invalid access value!"
 
     def load_encoding_images(self):
-        face_image_path = []
-
-        for i in self.credentials["Image"]:
-            face_image_path.append(("faces\\" + i))
-        # debug
-        print(face_image_path)
-
-        print("{} faces found.".format(len(face_image_path)))
-
-        # Store image encoding and names
-        for i in face_image_path:
-            initial_image = cv2.imread(i)
+        for index, row in self.credentials.iterrows():
+            self.names.append(row["Name"])
+            initial_image = cv2.imread(("faces\\" + row["Image"]))
             rgb_img = cv2.cvtColor(initial_image, cv2.COLOR_BGR2RGB)
-
-            # Get the filename only from the initial file path.
-            basename = os.path.basename(i)
-            (filename, ext) = os.path.splitext(basename)
-            # Get encoding
             image_encoding_endpoint = face_recognition.face_encodings(rgb_img)[0]
-
-            # Store file name and file encoding
             self.faces.append(image_encoding_endpoint)
-            self.names.append(filename)
+            self.access.append(row["Access Level"])
         print("Encoding faces loaded")
 
     def detect_known_faces(self, frame):
-        small_frame = cv2.resize(frame, (0, 0), fx=self.frame_resize_ratio, fy=self.frame_resize_ratio)
+        # access states 0 = denied, 1 = too low access level, 2 = granted
+        access_state = 0
+        frame_resized = cv2.resize(frame, (0, 0), fx=self.frame_resize_ratio, fy=self.frame_resize_ratio)
         # Find all the faces and face encodings in the current frame of video
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        resized_frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
+        face_locations = face_recognition.face_locations(resized_frame_rgb)
+        face_encodings = face_recognition.face_encodings(resized_frame_rgb, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(self.faces, face_encoding)
-            name = ""
 
-            # # If a match was found in faces, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = names[first_match_index]
-
-            # Or instead, use the known face with the smallest distance to the new face
+            """ uses the pre-existing face encoding and compares with the face in frame to get a euclidean distance
+            ref -> https://face-recognition.readthedocs.io/en/latest/face_recognition.html"""
             face_distances = face_recognition.face_distance(self.faces, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = self.names[best_match_index]
+            face_best_matching = np.argmin(face_distances)
+            if matches[face_best_matching] and \
+                    self.credentials.loc[f"{face_best_matching}", "Access Level"] >= self.access_level:
+                name = self.names[face_best_matching]
+                access_state = 2
+            elif matches[face_best_matching] and \
+                    self.credentials.loc[f"{face_best_matching}", "Access Level"] >= self.access_level:
+                name = self.names[face_best_matching]
+                access_state = 1
             else:
                 name = "Unknown Face Detected!"
             face_names.append(name)
@@ -72,4 +71,4 @@ class SimpleFacerec:
         # Convert to numpy array to adjust coordinates with frame resizing quickly
         face_locations = np.array(face_locations)
         face_locations = face_locations / self.frame_resize_ratio
-        return face_locations.astype(int), face_names
+        return face_locations.astype(int), face_names, access_state
